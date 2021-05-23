a2bd9d43824e36f8a622ff5ab1ec2eb4a1245f62
Sven Notermans <sven.notermans@live.nl>
2021-05-23 19:52:54 +0200

    added eventitem

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,14 @@
 
 import Head from "next/Head"
+import {useRouter} from "next/router"
 import React from 'react'
 import styles from "@/styles/Layout.module.css"
 import Header from "./Header"
 import Footer from "./Footer"
+import Showcase from "./Showcase"
 
 const Layout = ({title, keywords, description, children}) => {
+  const router = useRouter()
   return (
     <div>
       <Head>
@@ -14,6 +17,8 @@ const Layout = ({title, keywords, description, children}) => {
         <meta name="keywords" content={keywords} />
       </Head>
       <Header />
+
+      {router.pathname === '/' && <Showcase />}
       <div className={styles.container}>
         {children}
       </div>
